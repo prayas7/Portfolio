b2f2d588a3a0ffbdf318e344190db29deb1265bd
Prayas Gaur <prayasgaur.pg57@gmail.com>
2024-11-02 09:27:04 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,20 +5,22 @@ const Contact = () => {
     <footer className="contact" id="contact">
       <h1 style={{ fontSize: '4rem', color: 'white' }}>Contact</h1><br/>
       <div className="social-links">
-      <a href="prayasgaur.pg57@gmail.com" className="icon">
+        <a href="mailto:prayasgaur.pg57@gmail.com" className="icon">
           <i className="fas fa-envelope"></i> 
-       </a>
+        </a>
 
-      <a href="https://www.linkedin.com/in/prayas-gaur-b7b575267/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/prayas-gaur-b7b575267/" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-linkedin"></i>
         </a>
         <a href="https://github.com/prayas7" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-github"></i>
-      </a>
-        
+        </a>
       </div>
     </footer>
   );
 };
+
+  
+
 
 export default Contact;
